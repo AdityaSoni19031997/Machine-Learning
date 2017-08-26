d117cfa9c825a71856cdcf351cb2ae6a1f808c7a
Aditya Soni <soniaditya922@gmail.com>
2017-08-26 11:10:28 +0530

    Add files via upload

---
+++ Classifying_datasets/Convolutional_Neural_Networks/Convolutional_Neural_Networks/dogs_cats.py
@@ -0,0 +1,140 @@
+# Convolutional Neural Network
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 4000,
+                         epochs = 1,
+                         validation_data = test_set,
+                         validation_steps = 1000)
+
+# Part 3 - Making new predictions
+
+
+from keras.models import load_model
+
+classifier.save('dogs_cats.h5')
+
+
+import numpy as np
+from keras.preprocessing import image
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (1).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+print training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction
+
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (2).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (3).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (4).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (5).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_ (6).jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'dog'
+    print prediction
+else:
+    prediction = 'cat'
+    print prediction

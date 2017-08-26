71ac43ee63a1864c2713a667557ed5cbac34c3a8
Aditya Soni <soniaditya922@gmail.com>
2017-08-26 11:12:03 +0530

    Update dogs_cats.py

--- Classifying_datasets/Convolutional_Neural_Networks/Convolutional_Neural_Networks/dogs_cats.py
+++ Classifying_datasets/Convolutional_Neural_Networks/Convolutional_Neural_Networks/dogs_cats.py
@@ -13,13 +13,13 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(64, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
 
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
@@ -27,7 +27,7 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 512, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -55,10 +55,10 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 4000,
-                         epochs = 1,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 1000)
+                         validation_steps = 2000)
 
 # Part 3 - Making new predictions
 
